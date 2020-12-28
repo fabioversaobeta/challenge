f4cf9a8d9db72908241a370a5d7b03198f27e24a
Fabio Bandacheski <fabio.versao.beta@gmail.com>
2020-12-27 21:16:26 -0300

    routes

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\ImportController;
+
 use App\Http\Controllers\PeopleController;
 use App\Http\Controllers\PhonesController;
+
+use App\Http\Controllers\AddressController;
+use App\Http\Controllers\ItemsController;
+use App\Http\Controllers\OrdersController;
+use App\Http\Controllers\ItemOrderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +24,19 @@ use App\Http\Controllers\PhonesController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::post('/import', [ImportController::class, 'index']);
 
+/**
+ * People
+ */
 Route::get('/getPeople', [PeopleController::class, 'index']);
-
 Route::get('/getPhones', [PhonesController::class, 'index']);
+
+Route::get('/getAddress', [AddressController::class, 'index']);
+Route::get('/getItems', [ItemsController::class, 'index']);
+Route::get('/getOrders', [OrdersController::class, 'index']);
+Route::get('/getItemOrder', [ItemOrderController::class, 'index']);
